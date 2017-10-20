3e0579df24357c155df27ee4c47ce90d143ec99a
Robert Concepcion <robert.concepcion.iii@gmail.com>
2017-10-19 21:09:49 -0400

    add new section tab highlight feature that comes in/goes out accordingly, update material-ui and some other libs for small fixes and remove random devnotes file

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '1.0.6337' };
+let VersionInfo = { VERSION : '1.0.6791' };
 export default VersionInfo
