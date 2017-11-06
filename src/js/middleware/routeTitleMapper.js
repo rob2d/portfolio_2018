36fe525d71b6648f52d8d85d09023a506712a647
Robert Concepcion <robert.concepcion.iii@gmail.com>
2017-11-05 22:53:50 -0500

    improve window title text formatting a bit

--- src/js/middleware/routeTitleMapper.js
+++ src/js/middleware/routeTitleMapper.js
@@ -2,6 +2,8 @@ import { LOCATION_CHANGE } from 'react-router-redux/reducer'
 import { Sections } from 'constants/AppSections'
 import { projects } from 'strings'
 
+const SITE_NAME = `Rob's Portfolio`;
+
 const routeTitleMapper = store => next => action =>
 {   
     // if we detect an action of route changing,
@@ -11,11 +13,11 @@ const routeTitleMapper = store => next => action =>
         const { pathname } = action.payload;
         let pathIndex = Sections.findIndex((s)=>(s.basePath == pathname));
         if(pathIndex != -1) {
-            window.document.title = `${Sections[pathIndex].name} (Rob's Portfolio)`; 
+            window.document.title = `${SITE_NAME} -- ${Sections[pathIndex].name}`; 
         } else if(pathname.indexOf('projects/') != -1) {
             const projectId = pathname.substr(10, pathname.length); // cut off /projects/
             const projectIndex = projects.projectData.findIndex((p)=>(p.id==projectId));
-            window.document.title = projects.projectData[projectIndex].title + ' (Rob\'s Portfolio)';
+            window.document.title = `${SITE_NAME} -- ${projects.projectData[projectIndex].title}`;
         }
     }
     return next(action);
