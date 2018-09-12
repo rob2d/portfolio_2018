586f1d197547cb5aa7fe8a5a406fb6238b46f4c2
Robert Concepcion <robert.concepcion.iii@gmail.com>
2018-09-11 22:00:42 -0400

    never ended up expanding on routes, so clean that up (just a static front-end SPA for now)

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = new express.Router();
-
-routes.get('/', (req, res)=> {
-    
-});
-
-module.exports = routes;
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '1.0.12416' };
+let VersionInfo = { VERSION : '1.0.12419' };
 export default VersionInfo
