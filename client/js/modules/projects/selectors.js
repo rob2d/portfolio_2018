b8a4502aaa6756362e8b0067e1cbebb9d38e3907
Robert Concepcion <robert.concepcion.iii@gmail.com>
2018-12-06 01:11:28 -0500

    projectIdOfUrl selector

---
+++ client/js/modules/projects/selectors.js
@@ -0,0 +1,19 @@
+import defineSelector    from 'utils/defineSelector'
+
+const SECTION_ROOT = '/projects';
+
+const getProjectIdOfUrl = location => (  
+    !(location.pathname == SECTION_ROOT || 
+        location.pathname == '/'
+    ) ?
+    location.pathname.substr(SECTION_ROOT.length+1) : undefined
+);
+
+export const projectIdOfUrl = (state, { location }) => (
+    getProjectIdOfUrl(location)
+);
+
+
+export default { 
+    projectIdOfUrl
+}
