93585f74d0bba3d9efa66da051fe4cd66d5c37f4
Robert Concepcion <robert.concepcion.iii@gmail.com>
2018-12-16 16:49:22 -0500

    clean up code from last fix branch

--- client/js/utils/appHistory.js
+++ client/js/utils/appHistory.js
@@ -5,18 +5,20 @@ appHistory.goTo = function(url, e) {
     // extract actual event from React API
     const event = e.nativeEvent;
 
-    console.log('event ->', event);
+    // normal left click/touch behavior;
+    // push to app history
 
     if(!event.defaultPrevented && 
-      // Don't break middle-click or (meta|ctrl)+click (new tab)
-      !(event.button == 1 || event.button == 2 || event.metaKey || event.ctrlKey)
+      !(event.button == 1 || event.button == 2 || 
+       event.metaKey || event.ctrlKey)
     ) {
         appHistory.push({ pathname : url });
     } 
 
-    // if we discover that it was a middle click (or special click),
-    //  simply open URL in new tab
-    // and do not mess with router
+    // if we discover that it was a middle click 
+    // (or special click), simply open URL in new 
+    // tab and do not push to history
+
     else if(event.button == 1 || event.ctrlKey){
         window.open(url, '_blank'); 
     }
