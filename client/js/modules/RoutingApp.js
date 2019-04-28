ec9c839e53f2f1134203180e67a0adf5948ea625
Robert Concepcion <robert.concepcion.iii@gmail.com>
2019-04-28 17:15:03 -0400

    simplify and modernize conventions for several components/sections

--- client/js/modules/RoutingApp.js
+++ client/js/modules/RoutingApp.js
@@ -1,26 +1,26 @@
 import { Provider, connect } from 'react-redux'
 import React from 'react'
 import ThemeProvider from '@material-ui/styles/ThemeProvider'
-import withStyles from '@material-ui/styles/withStyles'
+import { makeStyles } from '@material-ui/styles'
 import Route from 'react-router-dom/Route'
 import Switch from 'react-router-dom/Switch'
 import { getTheme } from 'app-root/themeFactory'
 import appHistory from 'utils/appHistory'
-import withViewportSizes from 'utils/hocs/withViewportSizes'
 import { ConnectedRouter } from 'connected-react-router'
 import store from '../store'
 import lazyLoadComponent from 'utils/lazyLoadComponent'
 import { AppHeader, AppFooter } from './core'
 import { hot } from 'react-hot-loader/root'
+import useViewportSizes from 'use-viewport-sizes'
 
-const styles = theme => ({
+const useStyles = makeStyles(({ rc3 }) => ({
     appWrapper : {
         minHeight       : '100%',
         margin          : '0px auto',
         display         : 'flex',
         flexDirection   : 'row',
         textAlign       : 'center',
-        backgroundColor : theme.rc3.background,
+        backgroundColor : rc3.background,
         transition      : 'all 0.32s'
     },
     routeViewWrapper : {
@@ -33,7 +33,7 @@ const styles = theme => ({
         display : 'flex',
         flexDirection : 'column'
     }
-});
+}));
 
 const ProjectsPanel = lazyLoadComponent({
     loader : ()=> import( /* webpackChunkName: "projects" */
@@ -63,55 +63,50 @@ const CV = lazyLoadComponent({
     resolver : m => m.CV
 });
 
-const StyledContent = withStyles(styles)(
-    function AppContent ({ classes }) {
-        return (  
-            <div className={ classes.appWrapper }>
-                <div className={ classes.routeViewWrapper }>
-                    <AppHeader />
-                    {
-                        <Switch>
-                            <Route 
-                                exact path='(/about|/)' 
-                                component={ About } 
-                            />
-                            <Route 
-                                exact path='/cv' 
-                                component={ CV } 
-                            />
-                            <Route 
-                                path='(/projects|/projects/:projectId)' 
-                                component={ ProjectsPanel } 
-                            />
-                            <Route 
-                                path='(/misc)' 
-                                component={ Miscellaneous } 
-                            />
-                        </Switch>
-                    }
-                    <AppFooter />
-                </div>
+function AppContent () {
+    const classes = useStyles();
+
+    return (  
+        <div className={ classes.appWrapper }>
+            <div className={ classes.routeViewWrapper }>
+                <AppHeader />
+                    <Switch>
+                        <Route 
+                            exact path='(/about|/)' 
+                            component={ About } 
+                        />
+                        <Route 
+                            exact path='/cv' 
+                            component={ CV } 
+                        />
+                        <Route 
+                            path='(/projects|/projects/:projectId)' 
+                            component={ ProjectsPanel } 
+                        />
+                        <Route 
+                            path='(/misc)' 
+                            component={ Miscellaneous } 
+                        />
+                    </Switch>
+                <AppFooter />
             </div>
-        );
-    }
-);
+        </div>
+    );
+}
 
 
-const ThemedApp = withViewportSizes(connect(
-    ({ core })=> ({ theme : core.theme })
-)(
+const ThemedApp = connect(({ core : { theme } })=> ({ theme }))(
     function ThemedApp({ theme }) {
         const themeApplied = getTheme(theme); 
 
         return (
             <ThemeProvider theme={ themeApplied }>
                 <ConnectedRouter history={ appHistory }>
-                    <StyledContent />
+                    <AppContent />
                 </ConnectedRouter>
             </ThemeProvider>
         )
-    })
-);
+});
 
 
 function RoutingApp(){ 
