2418a416856f20f31793b291b571f74165763e7a
Robert Concepcion <robert.concepcion.iii@gmail.com>
2019-10-10 22:57:59 -0400

    add linting rules to play nicely with Node files, add \n to ROADMAP EOL

--- server/app.js
+++ server/app.js
@@ -1,74 +1,74 @@
+const path = require('path');
 
 // Possible command line arguments
 //
 // https    : whether to run in HTTPS mode
 // ssl_cert : location of ssl certificate file
-// ssl_key  : location of ssl key file 
-// ssl_ca   : location of ca file if using one 
+// ssl_key  : location of ssl key file
+// ssl_ca   : location of ca file if using one
 
 // ==================== //
 // PATH-RELATED GLOBALS //
 // ==================== //
 
-global.basePath = __dirname + '/';
+global.basePath = path.resolve(__dirname, '/');
 
 /**
  * Helper function which resolves to root
  * file __dirname/relPath
  */
-global.resolvePath = function(...args) {
-	return path.resolve(global.basePath, ...args);
-}
+global.resolvePath = function globalResolvePath(...args) {
+    return path.resolve(global.basePath, ...args);
+};
 
-global.require = function(...args) {
+global.require = function globalRequire(...args) {
     return require(global.resolvePath(...args));
-}
+};
 
 // ==================== //
 //  IMPORT/BASIC SETUP  //
 // ==================== //
 
-// Set up SSL, config a globally viewable 
-// basepath (__base), and require node 
+// Set up SSL, config a globally viewable
+// basepath (__base), and require node
 // modules
 
-const envFile = require('dotenv').config(), // funnel process.env vars
-      express = require('express'),
-      path = require('path'),
-      fs = require('fs'),
-      EventEmitter = require('events'),
-      app = express(),
-      http = require('http'),
-      https = require('https'),
-      cors = require('cors'),
-      helmet = require('helmet'),
-      argv = require('yargs').argv, //grabs our app arguments
-      colors = require('colors'),
-      bodyParser = require('body-parser'),
-      port = argv.port || process.env.PORT || 3002,
-      env = process.env.NODE_ENV || 'development';
-      
+const envFile = require('dotenv').config(); // funnel process.env vars
+const express = require('express');
+const fs = require('fs');
+const EventEmitter = require('events');
+const app = express();
+const http = require('http');
+const https = require('https');
+const cors = require('cors');
+const helmet = require('helmet');
+const { argv } = require('yargs'); //grabs our app arguments
+const colors = require('colors');
+const bodyParser = require('body-parser');
+const port = argv.port || process.env.PORT || 3002;
+const env = process.env.NODE_ENV || 'development';
 
-let sslConfig = (()=> {
+
+const sslConfig = (() => {
     if(argv.https) {
-        let certPath = argv.ssl_cert,
-            keyPath = argv.ssl_key,
-            caPath = argv.ssl_ca,
-            config = { https : true, options : {} };      
+        const certPath = argv.ssl_cert;
+        const keyPath = argv.ssl_key;
+        const caPath = argv.ssl_ca;
+        const config = { https : true, options : {} };
 
-            if(caPath) {
-                config.options.ca = fs.readFileSync(caPath);
-            }
-            else if(keyPath && certPath) {
-                config.options.key = fs.readFileSync(keyPath);
-                config.options.cert = fs.readFileSync(certPath);
-            }
-            else {
-                console.error('https flag requires "cert" and ' + 
-                                'key" or "ca" file parameters'
-                );
-                process.exit();
-            }
+        if(caPath) {
+            config.options.ca = fs.readFileSync(caPath);
+        }
+        else if(keyPath && certPath) {
+            config.options.key = fs.readFileSync(keyPath);
+            config.options.cert = fs.readFileSync(certPath);
+        }
+        else {
+            console.error('https flag requires "cert" and ' +
+                'key" or "ca" file parameters'
+            );
+            process.exit();
+        }
 
         return config;
     }
@@ -83,7 +83,7 @@ app.set('eventHandler', appCallbackHandler);
 
 // add connection ready event logger
 
-app.get('eventHandler').on('dbConnReady', function() {
+app.get('eventHandler').on('dbConnReady', () => {
     console.log('db connection ready event emitted!');
 });
 
@@ -98,21 +98,19 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ==================== //
 //  API ROUTES          //
 // ==================== //
 
-// require backend routes 
+// require backend routes
 
 //app.use('/api', require('./routes/api'));
-app.get('/api/*', function(req,res) {
+app.get('/api/*', (req,res) => {
     res.status(404).send('404 Error');
 });
-
 
 // ==================== //
 // FRONT END ROUTING    //
@@ -123,7 +121,7 @@ app.get('/api/*', function(req,res) {
 // with more time would send proper
 // code)
 
-app.get('*', function(req,res) {
+app.get('*', (req,res) => {
     res.render(
         path.join(__dirname,'public', 'index.html')
     );
@@ -133,39 +131,39 @@ app.get('*', function(req,res) {
 // START UP THE SERVER  //
 // ==================== //
 
-// Based on whether we're in SSL or not, 
-// launch a corresponding express server 
-// and give some an informative prompt to the 
+// Based on whether we're in SSL or not,
+// launch a corresponding express server
+// and give some an informative prompt to the
 // user.
 
-let startServer = server => {
-let host = server.address().address,
-    port = server.address().port,
-    version = require('./../package.json').version,
-    appName = require('./../package.json').name,
-    protocol = sslConfig.https ? 'https' : 'http';
+const startServer = server => {
+    const { address } = server.address();
+    const { port } = server.address();
+    const { version, name:appName } = require('./../package.json').version;
+    const protocol = sslConfig.https ? 'https' : 'http';
+    const host = (address == '0.0.0.0'|| address == '::') ? 'localhost' : address;
 
-    host = (host == '0.0.0.0'|| host == '::') ? 'localhost' : host;
+    console.log(`${'%s'.white}[v%s] %s`, appName, version, argv.dev ? '(DEV MODE)' : '');
+    console.log(
+        `-> currently running at ${protocol.blue.bold}:${`//%s:%s`.blue.bold}` +
+        ' in HTTP %s mode', host, port, sslConfig.https ? 'Secure' : 'Insecure'
+    );
 
-    console.log('%s '.white + '[v%s] %s', appName, version, argv.dev ? '(DEV MODE)' : '');
-    console.log('-> currently running at ' + protocol.blue.bold + '://%s:%s'.blue.bold +
-                ' in HTTP %s mode', host, port, sslConfig.https ? 'Secure' : 'Insecure');
-    
     if(!sslConfig.https) {
-        console.log('(to enable HTTPS, run app with the ' + 
+        console.log('(to enable HTTPS, run app with the ' +
             'flags "https", "ssl_cert" and "ssl_key")'
         );
     }
-    
+
     return server;
 };
 
-let protocolSrc = sslConfig.https ? https : http,
-    serverArgs = [ app ];
+const protocolSrc = sslConfig.https ? https : http;
+const serverArgs = [app];
 
 if(sslConfig.https) {
     serverArgs.splice(0,0,sslConfig.options);
 }
 
-let server = protocolSrc.createServer(...serverArgs)
-                    .listen(port, () => startServer(server) );
+const server = protocolSrc.createServer(...serverArgs)
+    .listen(port, () => startServer(server));
