6e79b29380a2c9ea508b5ee89163ec1df1255431
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-04-04 10:58:20 -0400

    createDerivedTheme: remove log which was adding calculation to theme changes

--- client/js/utils/themer/createDerivedTheme.js
+++ client/js/utils/themer/createDerivedTheme.js
@@ -3,13 +3,6 @@ import deepMerge from './deepMerge';
 export default function createDerivedTheme(themeDef) {
     themeDef = themeDef?.length ? themeDef : [themeDef];
 
-    console.log('themeDef derived ->', themeDef.reduce((themeDef = {}, xformer) =>
-        deepMerge(
-            typeof xformer == "function" ?
-                xformer(themeDef) : xformer, themeDef
-        ), {}
-    ));
-
     return themeDef.reduce((themeDef = {}, xformer) =>
         deepMerge(
             typeof xformer == "function" ?
