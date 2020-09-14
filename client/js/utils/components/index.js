99196dfcc2f63eef87400d3f7b185a86ac749cb8
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-09-13 20:13:43 -0400

    new abstraction components for PDF Views and Download Links to automate theming, debouncing, etc

--- client/js/utils/components/index.js
+++ client/js/utils/components/index.js
@@ -3,3 +3,5 @@
 export { default as ButtonLink } from './ButtonLink.js';
 export { default as LoadingComponent } from './LoadingComponent.js';
 export { default as OptimizedImg } from './OptimizedImg';
+export { default as PDFView } from './PDFView';
+export { default as PDFDownloadLink } from './PDFDownloadLink';
