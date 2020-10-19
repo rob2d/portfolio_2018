8c94736fa08cff26d4c7096964e293f60c95725c
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-10-18 21:54:19 -0400

    !misc: fix bug in theme object merging (for internal dynamic theming) + adjust eslint rules for no space after colon

--- client/js/utils/themer/deepMerge.js
+++ client/js/utils/themer/deepMerge.js
@@ -6,31 +6,29 @@
  * @param {Object} d destination object
  */
 function merge(s, d = {}) {
-	if (typeof s === "undefined") {
-		return d;
-	}
+    if(typeof s === 'undefined') { return d }
 
-	for (let key of Object.keys(s)) {
-		switch (typeof s[key]) {
-			case "object": {
-				d[key] = deepMerge(d[key], s[key]);
-				break;
-			}
+    for(const key of Object.keys(s)) {
+        switch (typeof s[key]) {
+            case 'object': {
+                d[key] = { ...deepMerge(d[key], s[key]) };
+                break;
+            }
 
-			case "number":
-			case "string": {
-				d[key] = s[key];
-				break;
-			}
-			default:
-			case "undefined": {
-				d[key] = deepMerge(s[key], {});
-				break;
-			}
-		}
-	}
+            case 'number':
+            case 'string': {
+                d[key] = s[key];
+                break;
+            }
+            default:
+            case 'undefined': {
+                d[key] = deepMerge(s[key], {});
+                break;
+            }
+        }
+    }
 
-	return d;
+    return d;
 }
 
 export default function deepMerge(s1, s2, d = {}) {
