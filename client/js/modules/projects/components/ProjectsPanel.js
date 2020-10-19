4af69dcd58820ac84ed3fff7bfcb52a433072156
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-10-18 23:02:38 -0400

    ProjectsPanel: do not apply scrollTo logic when back-nav out of projects, follow new syntax for linter *without* space before colon

--- client/js/modules/projects/components/ProjectsPanel.js
+++ client/js/modules/projects/components/ProjectsPanel.js
@@ -21,79 +21,79 @@ import useProjectIdOfUrl from '../hooks/useProjectIdOfUrl';
 const orderedProjects = projectOrder.map( id => projects[id] );
 
 const useStyles = makeStyles(() => ({
-    container : {
-        display : 'block',
-        position : 'relative',
-        flexGrow : 1,
-        maxWidth : '1100px',
-        margin : '0 auto',
-        top : '0px',
-        overflowX : 'hidden'
+    container: {
+        display: 'block',
+        position: 'relative',
+        flexGrow: 1,
+        maxWidth: '1100px',
+        margin: '0 auto',
+        top: '0px',
+        overflowX: 'hidden'
     },
-    content : {
-        display : 'flex',
-        alignItems : 'center',
-        justifyContent : 'center',
-        flexDirection : 'row',
-        flexWrap : 'wrap',
-        position : 'relative',
-        marginTop : '16px'
+    content: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        position: 'relative',
+        marginTop: '16px'
     },
     '@media (min-width: 600px)': {
-        mainContainer : {
-            padding : '32px',
+        mainContainer: {
+            padding: '32px',
         },
     },
     '@media (max-height: 480px)': {
-        mainContainer : {
-            paddingTop : '0px'
+        mainContainer: {
+            paddingTop: '0px'
         }
     },
-    emptyCard : {
-        display : 'block',
-        width : '300px',
-        height : '1px',
-        padding : '0px 32px'
+    emptyCard: {
+        display: 'block',
+        width: '300px',
+        height: '1px',
+        padding: '0px 32px'
     }
-}), { name : 'ProjectsPanel' });
+}), { name: 'ProjectsPanel' });
 
 const nextStateDict = {
-    [VIEW_ALL] : OFFSET_CALC,
-    [OFFSET_CALC] : AFTER_FADE_POSITIONING,
-    [AFTER_FADE_POSITIONING] : PROJECT_SCROLL_UP,
-    [PROJECT_SCROLL_UP] : PROJECT_VIEW
+    [VIEW_ALL]: OFFSET_CALC,
+    [OFFSET_CALC]: AFTER_FADE_POSITIONING,
+    [AFTER_FADE_POSITIONING]: PROJECT_SCROLL_UP,
+    [PROJECT_SCROLL_UP]: PROJECT_VIEW
 };
 
 const transitionTimes = {
-    [VIEW_ALL] : 50,
-    [OFFSET_CALC] : 25,
-    [AFTER_FADE_POSITIONING] : 0,
-    [PROJECT_SCROLL_UP] : 100
+    [VIEW_ALL]: 50,
+    [OFFSET_CALC]: 25,
+    [AFTER_FADE_POSITIONING]: 0,
+    [PROJECT_SCROLL_UP]: 100
 };
 
 const projectsReducer = (state, { type, payload }) => {
     switch (type) {
-        case 'selectProjectViaUI' :
+        case 'selectProjectViaUI':
             return {
                 ...state,
-                wasSelectionViaUI : true,
+                wasSelectionViaUI: true,
                 // begins transitions
-                displayState : nextStateDict[state.displayState] || state.displayState
+                displayState: nextStateDict[state.displayState] || state.displayState
             };
-        case 'advanceDisplayState' : {
+        case 'advanceDisplayState': {
             return {
                 ...state,
-                displayState : nextStateDict[state.displayState]
+                displayState: nextStateDict[state.displayState]
             };
         }
-        case 'viewAllProjects' : {
+        case 'viewAllProjects': {
             return {
                 ...state,
-                displayState : VIEW_ALL,
-                wasSelectionViaUI : false
+                displayState: VIEW_ALL,
+                wasSelectionViaUI: false
             };
         }
-        default : {
+        default: {
             return { ...state };
         }
     }
@@ -102,15 +102,15 @@ const projectsReducer = (state, { type, payload }) => {
 export default function ProjectsPanel() {
     const [projectId, location, match] = useProjectIdOfUrl();
     const prevProjectId = usePrevious(projectId);
-    useDocumentTitle({ title : !projectId && `${SITE_NAME} -- Projects` });
+    useDocumentTitle({ title: !projectId && `${SITE_NAME} -- Projects` });
 
     const [vpW, vpH] = useViewportSizes();
     const classes = useStyles();
     const fadeContainerClass = useAutoFaderClass();
 
     const initialState = useMemo(() => ({
-        displayState : !projectId ? VIEW_ALL : PROJECT_VIEW,
-        wasSelectionViaUI : false
+        displayState: !projectId ? VIEW_ALL: PROJECT_VIEW,
+        wasSelectionViaUI: false
     }), []);
 
     const [state, dispatch] = useReducer(projectsReducer, initialState);
@@ -124,36 +124,34 @@ export default function ProjectsPanel() {
         projectOrder.map( id =>
             [id, e => {
                 appHistory.goTo(`/projects/${id}`, e);
-                dispatch({ type : 'selectProjectViaUI', payload : id });
+                dispatch({ type: 'selectProjectViaUI', payload: id });
             }]
         )),
     []);
 
     useEffect(() => {
         if(!prevProjectId && projectId && !wasSelectionViaUI) {
-            dispatch({ type : 'selectProjectViaUI' });
+            dispatch({ type: 'selectProjectViaUI' });
         }
         else if(!projectId) {
-            dispatch({ type : 'viewAllProjects' });
+            dispatch({ type: 'viewAllProjects' });
         }
 
-        setTimeout(() => {
-            window.scrollTo({
-                top : 0,
-                left : 0,
-                behavior : 'smooth'
-            });
-        }, 250);
+        if(projectId) {
+            setTimeout(() => (
+                window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+            ), 250);
+        }
     }, [projectId]);
 
     useEffect(() => {
         switch (displayState) {
-            case VIEW_ALL :
+            case VIEW_ALL:
                 break;
-            default : {
+            default: {
                 wait(transitionTimes[displayState] || 200).then(() => {
                     if(displayState != PROJECT_VIEW) {
-                        dispatch({ type : 'advanceDisplayState' });
+                        dispatch({ type: 'advanceDisplayState' });
                     }
                 });
                 break;
