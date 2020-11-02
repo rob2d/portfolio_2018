188ce62ca0d610e170b797134d1ece4d30cd8e58
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-11-02 16:31:21 -0500

    fix: remove deprecated export

--- client/js/utils/index.js
+++ client/js/utils/index.js
@@ -1,7 +1,6 @@
 // @create-index
 
 export { default as appHistory } from './appHistory.js';
-export { default as defineSelector } from './defineSelector.js';
 export { default as isLandscape } from './isLandscape.js';
 export { default as isPortrait } from './isPortrait.js';
 export { default as shouldShowHoverContent } from './shouldShowHoverContent.js';
