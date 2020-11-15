44a4a363b114404e7f10046ab4771576ba8b624d
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2020-11-15 04:03:22 -0500

    update use-viewport-sizes project description and add yarn repo link

--- client/js/data/projects/vpsizes.js
+++ client/js/data/projects/vpsizes.js
@@ -4,9 +4,14 @@ export default {
     year: '2019-2020',
     context: 'Independent',
     roles: 'Developer, Maintainer',
-    technologies: ['js','npm', 'react'],
-    platforms: ['web'],
+    technologies: ['js', 'react'],
     shortDescription: 'A simple and efficient React hook for measuring the window\'s size',
+    description: [
+        'A simple and efficient React hook for measuring the window\'s ' +
+        'size with support for serverside rendering and debouncing. ' +
+        'Downloaded over 100,000 times by other developers via Node Package Manager, ' +
+        'and used in several widely used frontend applications.'
+    ],
     sourceCode: [
         {
             url: 'https://github.com/rob2d/use-viewport-sizes',
@@ -15,8 +20,12 @@ export default {
     ],
     links: [
         {
+            url: 'https://yarnpkg.com/package/use-viewport-sizes',
+            description: 'Yarn repository/Readme'
+        },
+        {
             url: 'https://preview.npmjs.com/package/use-viewport-sizes',
-            description: 'NPM Package'
+            description: 'NPM repository/Readme'
         }
     ]
 };
