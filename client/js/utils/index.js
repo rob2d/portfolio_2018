c9d75ae31a45180f5216ac66c26ed4c40d685044
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-04-25 16:47:00 -0400

    rewrite appHistory into hook to support ReactRouter v6 paradigm

--- client/js/utils/index.js
+++ client/js/utils/index.js
@@ -1,5 +1,5 @@
 // @create-index
 
-export { default as appHistory } from './appHistory.js';
+export { default as useNavigateTo } from './navigateTo.js';
 export { default as shouldShowHoverContent } from './shouldShowHoverContent.js';
 export { default as wait } from './wait.js';
---
+++ client/js/utils/navigateTo.js
@@ -0,0 +1,51 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+/**
+ * a hook that abstracts `useNavigate`/
+ * returns a method that we can also pass a mouse
+ * event to handle middle-clicks properly
+ */
+export default function useNavigateTo() {
+    const navigate = useNavigate();
+
+    const navigateTo = useCallback((url, e) => {
+
+        // extract actual event from React API
+
+        const event = e.nativeEvent;
+        const isAbsoluteUrl = (
+            (url.indexOf('https://') != -1) ||
+        (url.indexOf('http://') != -1) ||
+        (url.indexOf('mailto:') != -1)
+        );
+
+        // normal left click/touch behavior;
+
+        if(!event.defaultPrevented &&
+      !(event.button == 1 || event.button == 2 ||
+       event.metaKey || event.ctrlKey)
+        ) {
+        // push to app history for in-app routing
+
+            if(!isAbsoluteUrl) {
+                navigate(url);
+            }
+            // open in new tab
+
+            else {
+                window.open(url, '_blank');
+            }
+        }
+
+        // if we discover that it was a middle click
+        // (or special click), simply open URL in new
+        // tab and do not push to history
+
+        else if((event.button == 1 || event.ctrlKey)) {
+            window.open(url, '_blank');
+        }
+    }, [navigate]);
+
+    return navigateTo;
+}
