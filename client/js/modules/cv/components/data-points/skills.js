9a3ea7faadb88c0363582aab5afe2795937c3bd0
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-05-16 19:11:07 -0400

    add projects info, update experience, refactor

---
+++ client/js/modules/cv/components/data-points/skills.js
@@ -0,0 +1,48 @@
+const skills = [{
+    name: 'Engineering',
+    items: [[
+        'ES 5/6/7+',
+        'TypeScript'
+    ], [
+        'React',
+        'Rxjs',
+        'Angular',
+    ],
+    [
+        'Redux',
+        'NgRX',
+        'HTML5'
+    ],
+    [
+        'Node',
+        'Webpack',
+        'Mongo'
+    ],
+    [
+        'CircleCI',
+        'Jenkins',
+        'MySQL',
+    ],
+    [
+        'Git',
+        'Puppeteer',
+        'Java'
+    ],
+    [
+        'CSS',
+        'Android SDK',
+    ]],
+},
+{
+    name: 'Multimedia',
+    items: [[
+        'Photoshop',
+        'Krita',
+    ],[
+
+        'Resolve',
+        'Blender3D',
+    ]]
+}];
+
+export default skills;
