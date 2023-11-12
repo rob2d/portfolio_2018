b8c0c0ca3cd70727b38de983983ac0c65b2c3c7c
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-11-11 21:10:54 -0500

    update viewport sizes info to include tests

--- client/js/data/projects/vpsizes.js
+++ client/js/data/projects/vpsizes.js
@@ -4,13 +4,13 @@ export default {
     year: '2019, 2023',
     context: 'Independent',
     roles: 'Developer, Maintainer',
-    technologies: ['js', 'react'],
-    shortDescription: 'A simple and efficient React hook for measuring the window\'s size',
+    technologies: ['js', 'react', 'jest'],
+    shortDescription: 'A simple, efficient, fully-tested typescript-compatible React hook for measuring the window\'s size',
     description: [
-        'A simple and efficient React hook for measuring the window\'s ' +
-        'size with support for serverside rendering and debouncing. ' +
-        'Downloaded over 100,000 times by other developers via Node Package Manager, ' +
-        'and used in several widely used frontend applications.'
+        'A simple, efficient, fully-tested typescript-compatible React hook for measuring the window\'s ' +
+        'size with support for serverside rendering and debouncing.',
+        'Collectively it has had over 100,000 downloads via Node Package Manager since written, ' +
+        'and was used in several widely used frontend applications.'
     ],
     media: {
         aspectRatio: 1.2944,
